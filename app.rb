c77b6eb671f0316de3c1b93f5cfe435cadf91b9e
Jozy Kinnaman <jozypants@gmail.com>
2020-05-22 14:26:19 -0700

    add routes and views for adding and viewing volunteers.

--- app.rb
+++ app.rb
@@ -45,3 +45,40 @@ patch('/projects/:id') do
   @projects = Project.all
   erb(:projects)
 end
+
+delete('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.delete()
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects/:id/volunteers/:volunteer_id') do
+  @volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  erb(:volunteer)
+end
+
+post('/projects/:id/volunteers') do
+  @project = Project.find(params[:id].to_i())
+  name = params[:volunteer_name]
+  project_id = @project.id
+  volunteer = Volunteer.new({name: name, id: nil, project_id: project_id})
+  volunteer.save()
+  erb(:project)
+end
+
+# Edit a volunteer and then route back to the project view.
+patch('/projects/:id/volunteers/:volunteer_id') do
+  @project = Project.find(params[:id].to_i())
+  volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  volunteer.update(params[:name], @project.id)
+  erb(:project)
+end
+
+# Delete a volunteer and then route back to the project view.
+delete('/projects/:id/volunteers/:volunteer_id') do
+  volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  volunteer.delete
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
+end
