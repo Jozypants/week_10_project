a3d7e0621ec0a8165b03b3bd0c47c1010b834050
Jozy Kinnaman <jozypants@gmail.com>
2020-05-22 12:15:54 -0700

    highlight test for '#volunteers' expect to PASS.

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -62,17 +62,17 @@ describe Project do
     end
   end
 
-  # describe '#volunteers' do
-  #   it 'returns all volunteers for a specific project' do
-  #     project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-  #     project.save
-  #     volunteer1 = Volunteer.new({:name => 'Jasmine', :project_id => project.id, :id => nil})
-  #     volunteer1.save
-  #     volunteer2 = Volunteer.new({:name => 'Joe', :project_id => project.id, :id => nil})
-  #     volunteer2.save
-  #     expect(project.volunteers).to eq [volunteer1, volunteer2]
-  #   end
-  # end
+  describe '#volunteers' do
+    it 'returns all volunteers for a specific project' do
+      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      project.save
+      volunteer1 = Volunteer.new({:name => 'Jasmine', :project_id => project.id, :id => nil})
+      volunteer1.save
+      volunteer2 = Volunteer.new({:name => 'Joe', :project_id => project.id, :id => nil})
+      volunteer2.save
+      expect(project.volunteers).to eq [volunteer1, volunteer2]
+    end
+  end
 
   describe '#update' do
     it 'allows a user to update a project' do
@@ -91,17 +91,5 @@ describe Project do
       expect(Project.all).to eq []
     end
   end
-
-  # describe('#volunteers') do
-  #   it("returns a project's volunteers") do
-  #     project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-  #     project.save
-  #     volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-  #     volunteer1.save
-  #     volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
-  #     volunteer2.save
-  #     expect(project.volunteers).to(eq([volunteer1, volunteer2]))
-  #   end
-  # end
 
 end
