afe22962cb4c17da558f2b0a9add9404d5881295
Jozy Kinnaman <jozypants@gmail.com>
2020-05-22 13:40:12 -0700

    add routes and views for adding and viewing projects.

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
-require('.lib/project')
-require('.lib/volunteer')
+require('./lib/project')
+require('./lib/volunteer')
 require('pry')
 require("pg")
 DB = PG.connect({:dbname => "volunteer_tracker"})
@@ -15,4 +15,21 @@ end
 get('/projects') do
   @projects = Project.all
   erb(:projects)
+end
+
+get('/projects/new') do
+  erb(:new_project)
+end
+
+post('/projects') do
+  title = params[:project_title]
+  project = Project.new({title: title, id: nil})
+  project.save()
+  @projects = Project.all()
+  erb(:projects)
+end
+
+get('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
 end
